a9c35eabad3d5e0af1a0b827622d606f900890c9
hyunsu <rkdgustn77@naver.com>
2024-07-23 21:41:26 +0900

    Revert "feat: account signup 개발진행중"
    This reverts commit a9eef6b538b70322330c442e0bba4faafe34ae4c.

--- src/main/kotlin/org/store/clothstar/member/service/MemberService.kt
+++
@@ -1,7 +0,0 @@
-package org.store.clothstar.member.service
-
-import org.store.clothstar.dto.request.CreateMemberRequest
-
-interface MemberService {
-    fun signUp(createMemberDTO: CreateMemberRequest)
-}
