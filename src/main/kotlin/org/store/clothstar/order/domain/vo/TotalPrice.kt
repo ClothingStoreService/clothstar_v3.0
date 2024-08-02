292b23a3b405737b06659d5222a21847d185e88f
subin <axhtl@gachon.ac.kr>
2024-08-02 16:39:16 +0900

    refactor: TotalPrice - shipping 필드에 주석 추가

--- src/main/kotlin/org/store/clothstar/order/domain/vo/TotalPrice.kt
+++ src/main/kotlin/org/store/clothstar/order/domain/vo/TotalPrice.kt
@@ -6,7 +6,7 @@ import jakarta.persistence.Embeddable
 @Embeddable
 data class TotalPrice(
     @Column(name = "total_shipping_price", nullable = false)
-    var shipping: Int = 3000,
+    var shipping: Int = 3000, // 기본 배송비는 3000원.
 
     @Column(name = "total_products_price", nullable = false)
     var products: Int = 0,
