ccaaa1316960ffb6fc868edd5e0ddc7112cfd7b1
Ogu1208 <kdasunb6@gmail.com>
2024-08-04 09:01:42 +0900

    test: CategoryService Test

--- src/main/kotlin/org/store/clothstar/category/service/CategoryService.kt
+++ src/main/kotlin/org/store/clothstar/category/service/CategoryService.kt
@@ -37,19 +37,19 @@ class CategoryService(
             .also {
                 log.info { "Category created, id : ${category.categoryId}" }
             }
+    }
 
-        @Transactional
-        fun updateCategory(categoryId: Long, updateCategoryRequest: UpdateCategoryRequest) {
-            val category = categoryRepository.findById(categoryId)
-                .orElseThrow { ResponseStatusException(HttpStatus.BAD_REQUEST, "해당 카테고리를 찾을 수 없습니다.") }
-            category.updateCategory(updateCategoryRequest)
-        }
+    @Transactional
+    fun updateCategory(categoryId: Long, updateCategoryRequest: UpdateCategoryRequest) {
+        val category = categoryRepository.findById(categoryId)
+            .orElseThrow { ResponseStatusException(HttpStatus.BAD_REQUEST, "해당 카테고리를 찾을 수 없습니다.") }
+        category.updateCategory(updateCategoryRequest)
+    }
 
-        @Transactional
-        fun deleteCategory(categoryId: Long) {
-            val category = categoryRepository.findById(categoryId)
-                .orElseThrow { ResponseStatusException(HttpStatus.BAD_REQUEST, "해당 카테고리를 찾을 수 없습니다.") }
-            categoryRepository.delete(category)
-        }
+    @Transactional
+    fun deleteCategory(categoryId: Long) {
+        val category = categoryRepository.findById(categoryId)
+            .orElseThrow { ResponseStatusException(HttpStatus.BAD_REQUEST, "해당 카테고리를 찾을 수 없습니다.") }
+        categoryRepository.delete(category)
     }
 }
