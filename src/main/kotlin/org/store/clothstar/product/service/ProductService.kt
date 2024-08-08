54e212a264fcbe6aa33319f9c49cedd528c26881
hyunsu <rkdgustn77@naver.com>
2024-08-09 01:43:43 +0900

    feat: 소스정리

--- src/main/kotlin/org/store/clothstar/product/service/ProductService.kt
+++ src/main/kotlin/org/store/clothstar/product/service/ProductService.kt
@@ -2,19 +2,12 @@ package org.store.clothstar.product.service
 
 import org.springframework.stereotype.Service
 import org.store.clothstar.product.domain.Product
-import org.store.clothstar.product.repository.ItemRepository
-import org.store.clothstar.product.repository.OptionValueRepository
-import org.store.clothstar.product.repository.ProductOptionRepository
 import org.store.clothstar.product.repository.ProductRepository
 
 @Service
 class ProductService(
     private val productRepository: ProductRepository,
-    private val productOptionRepository: ProductOptionRepository,
-    private val optionValueRepository: OptionValueRepository,
-    private val itemRepository: ItemRepository,
 ) {
-
     fun findByIdIn(productIds: List<Long>): List<Product> {
         return productRepository.findByProductIdIn(productIds).map {
             it ?: throw IllegalArgumentException("상품을 조회할 수 없습니다.")
