dbe8d6eb5407c23ada6bd61fa1b8e5680b0f5369
hyunsu <rkdgustn77@naver.com>
2024-08-10 02:22:11 +0900

    feat: 소스 수정

--- src/main/kotlin/org/store/clothstar/order/dto/response/OrderDetailResponse.kt
+++ src/main/kotlin/org/store/clothstar/order/dto/response/OrderDetailResponse.kt
@@ -5,38 +5,38 @@ import io.swagger.v3.oas.annotations.media.Schema
 @Schema(description = "주문 상세 조회용 Response")
 class OrderDetailResponse(
     @Schema(description = "주문 상세 번호", example = "1")
-    private val orderDetailId: Long,
+    val orderDetailId: Long,
 
     @Schema(description = "주문 번호", example = "1")
-    private val orderId: Long,
+    val orderId: Long,
 
     @Schema(description = "상품 번호", example = "1")
-    private val productLineId: Long,
+    val productLineId: Long,
 
     @Schema(description = "상품 옵션 번호", example = "1")
-    private val productId: Long,
+    val productId: Long,
 
     @Schema(description = "상품 수량", example = "2")
-    private val quantity: Int = 0,
+    val quantity: Int = 0,
 
     @Schema(description = "고정 가격", example = "15000")
-    private val fixedPrice: Int = 0,
+    val fixedPrice: Int = 0,
 
     @Schema(description = "상품 종류 하나당 총 가격", example = "30000")
-    private val oneKindTotalPrice: Int = 0,
+    val oneKindTotalPrice: Int = 0,
 
     @Schema(description = "상품 이름", example = "나이키 반팔티")
-    private val name: String,
+    val name: String,
 
     @Schema(description = "옵션 상품 재고", example = "30")
-    private val stock: Long,
+    val stock: Long,
 
     @Schema(description = "옵션 이름", example = "검정")
-    private val optionName: String,
+    val optionName: String,
 
     @Schema(description = "옵션 추가 비용", example = "0")
-    private val extraCharge: Int,
+    val extraCharge: Int,
 
     @Schema(description = "브랜드 이름", example = "나이키")
-    private val brandName: String,
+    val brandName: String,
 )
