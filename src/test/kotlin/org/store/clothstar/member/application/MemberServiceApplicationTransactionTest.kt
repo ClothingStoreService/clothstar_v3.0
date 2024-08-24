c32fb1fc8d17c57e6fee5c08f4617881f58745b3
hyunsu <rkdgustn77@naver.com>
2024-08-24 15:37:59 +0900

    feat: member Delete 테스트코드 작성

--- src/test/kotlin/org/store/clothstar/member/application/MemberServiceApplicationTransactionTest.kt
+++ src/test/kotlin/org/store/clothstar/member/application/MemberServiceApplicationTransactionTest.kt
@@ -7,10 +7,13 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mockito.doThrow
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.boot.test.mock.mockito.SpyBean
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.transaction.annotation.Transactional
@@ -19,6 +22,7 @@ import org.store.clothstar.common.error.exception.NotFoundMemberException
 import org.store.clothstar.member.authentication.service.AuthenticationService
 import org.store.clothstar.member.domain.Account
 import org.store.clothstar.member.domain.Member
+import org.store.clothstar.member.domain.MemberRole
 import org.store.clothstar.member.repository.AccountRepository
 import org.store.clothstar.member.repository.MemberRepository
 import org.store.clothstar.member.service.AccountService
@@ -37,7 +41,7 @@ class MemberServiceApplicationTransactionTest {
     @Autowired
     lateinit var em: EntityManager
 
-    @Autowired
+    @SpyBean
     lateinit var accountService: AccountService
 
     @Autowired
@@ -82,5 +86,24 @@ class MemberServiceApplicationTransactionTest {
         //then
         assertThat(account.deletedAt).isNotNull()
         assertThat(member.deletedAt).isNotNull()
+    }
+
+    @DisplayName("회원을 삭제하다가 에러나면 전부 롤백되는지 확인한다.")
+    @Test
+    fun deleteFail_TransactionTest() {
+        doThrow(IllegalArgumentException()).`when`(accountService).updateDeletedAt(memberId, MemberRole.USER)
+        assertThrows<IllegalArgumentException> { memberServiceApplication.updateDeleteAt(memberId) }
+        em.flush()
+        em.clear()
+
+        val account = accountRepository.findByIdOrNull(accountId)
+            ?: throw NotFoundMemberException(ErrorCode.NOT_FOUND_ACCOUNT)
+
+        val member = memberRepository.findByIdOrNull(memberId)
+            ?: throw NotFoundMemberException(ErrorCode.NOT_FOUND_MEMBER)
+
+        //then
+        assertThat(account.deletedAt).isNull()
+        assertThat(member.deletedAt).isNull()
     }
 }
