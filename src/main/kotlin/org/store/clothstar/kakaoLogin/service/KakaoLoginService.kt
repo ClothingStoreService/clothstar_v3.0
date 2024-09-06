827de7332bdecc58c260d6445df87e4aeb61de27
subin <axhtl@gachon.ac.kr>
2024-09-07 08:17:57 +0900

    refactor: 사용하지 않는 요청 삭제

--- src/main/kotlin/org/store/clothstar/kakaoLogin/service/KakaoLoginService.kt
+++ src/main/kotlin/org/store/clothstar/kakaoLogin/service/KakaoLoginService.kt
@@ -9,8 +9,6 @@ import org.springframework.util.LinkedMultiValueMap
 import org.springframework.util.MultiValueMap
 import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.client.WebClient
-import org.store.clothstar.kakaoLogin.dto.KakaoRenewTokenResponseDto
-import org.store.clothstar.kakaoLogin.dto.KakaoTokenInfoResponseDto
 import org.store.clothstar.kakaoLogin.dto.KakaoTokenResponseDto
 import org.store.clothstar.kakaoLogin.dto.KakaoUserInfoResponseDto
 
@@ -85,60 +83,5 @@ class KakaoLoginService {
 
         logger.info { "email : ${userInfo.kakaoAccount!!.email}" }
         return userInfo
-    }
-
-    // 토큰 유효성 검사
-    fun validateToken(accessToken: String, refreshToken: String): KakaoRenewTokenResponseDto {
-        return try {
-            getTokenInfo(accessToken)
-            // 액세스 토큰이 유효한 경우 토큰 갱신
-            refreshAccessToken(refreshToken)
-        } catch (e: Exception) {
-            // getTokenInfo에서 예외가 발생한 경우 재로그인 필요
-            throw Exception("Access Token이 만료되었습니다. 재로그인이 필요합니다.")
-        }
-    }
-
-    // 토큰 정보 보기
-    fun getTokenInfo(accessToken: String): KakaoTokenInfoResponseDto {
-        val response = WebClient.create("https://kapi.kakao.com")
-            .get()
-            .uri("/v1/user/access_token_info")
-            .header("Authorization", "Bearer $accessToken")
-            .retrieve()
-            .bodyToMono(String::class.java)
-            .block()
-
-        val objectMapper = ObjectMapper()
-        val tokenInfo: KakaoTokenInfoResponseDto =
-            objectMapper.readValue(response, KakaoTokenInfoResponseDto::class.java)
-
-        logger.info { "Access Token 만료기한 : ${tokenInfo.expiresIn}" }
-        return tokenInfo
-    }
-
-    // 리프레시 토큰으로 액세스 토큰 갱신
-    fun refreshAccessToken(refreshToken: String): KakaoRenewTokenResponseDto {
-        val params: MultiValueMap<String, String> = LinkedMultiValueMap()
-        params.add("grant_type", "refresh_token")
-        params.add("client_id", clientId)
-        params.add("refresh_token", refreshToken)
-        params.add("client_secret", clientSecret)
-
-        val response = WebClient.create("https://kauth.kakao.com")
-            .post()
-            .uri(tokenUri)
-            .body(BodyInserters.fromFormData(params))
-            .header("Content-Type", "application/x-www-form-urlencoded;charset=utf-8")
-            .retrieve()
-            .bodyToMono(String::class.java)
-            .block()
-
-        val objectMapper = ObjectMapper()
-        val kakaoToken: KakaoRenewTokenResponseDto =
-            objectMapper.readValue(response, KakaoRenewTokenResponseDto::class.java)
-
-        logger.info { "갱신된 Access Token : ${kakaoToken.accessToken}" }
-        return kakaoToken
     }
 }
